b0d2a03ac6d24c6e319b71385b2b8646d232475e
Akuma_Yu <manjusaka1110@gmail.com>
2025-05-19 14:48:42 +0800

    運用include & require  做檔案拆分， GET和file_exists做到頁面跳轉精簡以及增加使用??方式精簡isset

---
+++ include/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>學生管理系統</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <?php require("header.php");?>
+    <?php include("nav.php");?>
+    <?php
+        $page=isset($_GET['page']) ? $_GET['page'] : 'main';
+        $page=$_GET['page']  ??  'main';
+        $file=$page.".php";
+
+        if(file_exists($file)) {
+            include $file;
+        }else{
+            include "main.php";
+        }
+        
+        // switch($page) {
+        //     case 'list':
+        //         include "list.php";
+        //         break;
+        //     case 'new':
+        //         include "new.php";
+        //         break;
+        //     case 'query':
+        //         include "query.php";
+        //         break;
+        //     case 'about':
+        //         include "about.php";
+        //         break;
+        //     default:
+        //         include "main.php";
+        // }
+    
+    ?>
+    <?php include("footer.php") ?>
+</body>
+</html>

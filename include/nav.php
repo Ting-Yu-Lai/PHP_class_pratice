d43485f04f03b123e7795894ab2aa4a80ea21763
Akuma_Yu <manjusaka1110@gmail.com>
2025-05-19 15:28:21 +0800

    增家背景反白，字體顏色，各頁面單獨做反白

--- include/nav.php
+++ include/nav.php
@@ -1,9 +1,9 @@
     <nav>
         <ul>
-            <li><a href="index.php">首頁</a></li>
-            <li><a href="index.php?page=list">學生列表</a></li>
-            <li><a href="index.php?page=new">新增學生</a></li>
-            <li><a href="index.php?page=query">查詢學生</a></li>
-            <li><a href="index.php?page=about">關於</a></li>
+            <li><a class="<?=($page=='main') ? 'activate':'';?>" href="index.php">首頁</a></li>
+            <li><a class="<?=($page=='list') ? 'activate':'';?>" href="index.php?page=list">學生列表</a></li>
+            <li><a class="<?=($page=='new') ? 'activate':'';?>" href="index.php?page=new">新增學生</a></li>
+            <li><a class="<?=($page=='query') ? 'activate':'';?>" href="index.php?page=query">查詢學生</a></li>
+            <li><a class="<?=($page=='about') ? 'activate':'';?>" href="index.php?page=about">關於</a></li>
         </ul>
     </nav>
